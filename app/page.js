551bb04a57b7c49386492e9328d3f30164d6b9df
Pancake <148641211+PancakeTaste@users.noreply.github.com>
2023-12-17 03:36:24 +0200

    Firebase

--- app/page.js
+++ app/page.js
@@ -1,8 +1,40 @@
+"use client";
+import { useState } from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Image from 'next/image';
 
 export default function Home() {
     const inputClass = "bg-transparent py-2 px-4 border-4 border-amber-500 placeholder-amber-100 text-lg font-bold rounded-xl outline-none focus:bg-amber-600 focus:placeholder-white transition";
     const buttonClass = "py-2 px-4 text-lg font-bold rounded-xl transition";
+
+    const [loading, setLoading] = useState(0);
+
+    const [ nickname, setNickname ] = useState('');
+    const handleNicknameChange = (e) => {
+        setNickname(e.target.value);
+    };
+
+    const notifyError = (message) => {
+        toast.error(message);
+    };
+
+    const checkNickname = () => {
+        if (nickname === '') {
+            notifyError('Pick a cool nickname.');
+            return false;
+        }
+        return true;
+    };
+
+    const handleAction = (type) => {
+        if (!checkNickname()) {
+            return;
+        }
+        setLoading(type);
+    };
 
     return (
         <>
@@ -14,19 +46,36 @@ export default function Home() {
                         type="text"
                         placeholder="Nickname"
                         className={inputClass}
+                        onKeyUp={handleNicknameChange}
                     />
 
-                    <button className={`border-4 border-red-500 bg-red-500 hover:bg-red-600 ${buttonClass}`}>
-                        Start a new game
+                    <button
+                        className={`border-4 border-red-500 bg-red-500 hover:bg-red-600 ${buttonClass}`}
+                        onClick={() => handleAction(1)}
+                    >
+                        { loading === 1 ? 'Creating new game...' : 'Start a new game' }
                     </button>
-                    <button className={`border-4 border-green-500 bg-green-500 hover:bg-green-600 ${buttonClass}`}>
-                        Join a game
+                    <button
+                        className={`border-4 border-green-500 bg-green-500 hover:bg-green-600 ${buttonClass}`}
+                        onClick={() => handleAction(2)}
+                    >
+                        { loading === 2 ? 'Joining game...' : 'Join a game' }
                     </button>
-                    <button className={`border-4 border-cyan-500 bg-cyan-500 hover:bg-cyan-600 ${buttonClass}`}>
-                        Find a game
+                    <button
+                        className={`border-4 border-cyan-500 bg-cyan-500 hover:bg-cyan-600 ${buttonClass}`}
+                        onClick={() => handleAction(3)}
+                    >
+                        { loading === 3 ? 'Finding game...' : 'Find a game' }
                     </button>
                 </div>
             </div>
+
+            <ToastContainer
+                position="bottom-center"
+                autoClose={3000}
+                draggable={false}
+                theme="dark"
+            />
         </>
     );
 }
