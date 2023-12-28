78ac08b01d55063e419ec30cb002de59ba94a1b9
Pancake <148641211+PancakeTaste@users.noreply.github.com>
2023-12-28 16:05:32 +0200

    Create route.js

---
+++ app/api/timestamp/route.js
@@ -0,0 +1,13 @@
+export async function GET() {
+    const timestamp = Math.floor(Date.now() / 1000);
+
+    const headers = {
+        'Cache-Control': 'no-store', // or 'max-age=0' to indicate no caching
+    };
+
+    return NextResponse.json({
+        timestamp
+    }, {
+        headers
+    });
+}
