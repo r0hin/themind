993f1919d51c6fd8fc97f47cb17ec094046192c5
Pancake <148641211+PancakeTaste@users.noreply.github.com>
2023-12-28 16:16:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig
