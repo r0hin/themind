7aa9abe9fbfc515683eb8cc3ddf1bbab34cc98b8
Pancake <148641211+PancakeTaste@users.noreply.github.com>
2023-12-28 16:16:02 +0200

    Update date.js

--- utils/date.js
+++ utils/date.js
@@ -1,5 +1,5 @@
 export async function getTimestamp() {
-    const response = await fetch('/api/timestamp');
+    const response = await fetch('/api/timestamp', { cache: 'no-store' });
     const data = await response.json();
     return data.timestamp;
 }
